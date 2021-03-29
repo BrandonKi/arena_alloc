ff318ec473d8d74fbcc099b83789e85864cb8f68
Brandon Kirincich <44515121+BrandonKi@users.noreply.github.com>
2021-03-29 00:06:49 -0400

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -7,16 +7,16 @@
 #include "arena_alloc.h"
 
 int main() {
-	allocator<int> alloc;
-	std::vector<int, allocator<int>> vec1(100, alloc);
+	arena_allocator<int> alloc;
+	std::vector<int, arena_allocator<int>> vec1(100, alloc);
 	std::iota(vec1.begin(), vec1.end(), 0);
 	std::for_each(vec1.cbegin(), vec1.cend(), [](int i) { std::cout << i << ','; });
 
-	std::vector<int, allocator<int>> vec2(100, alloc);
+	std::vector<int, arena_allocator<int>> vec2(100, alloc);
 	std::iota(vec2.begin(), vec2.end(), 0);
 	std::for_each(vec2.cbegin(), vec2.cend(), [](int i) { std::cout << i << ','; });
 
-	std::vector<int, allocator<int>> vec3(200, alloc);
+	std::vector<int, arena_allocator<int>> vec3(200, alloc);
 	std::iota(vec3.begin(), vec3.end(), 0);
 	std::for_each(vec3.cbegin(), vec3.cend(), [](int i) { std::cout << i << ','; });
 }
